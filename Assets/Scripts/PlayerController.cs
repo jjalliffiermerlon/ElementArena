60c05f22a19803f3f9cc4763039aa34526fc1e26
Aelita Le ROlland <arthur.lerolland@live.fr>
2023-11-25 12:09:59 +0100

    Link PlayerController to Player Script

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -3,40 +3,32 @@ using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
-    //Variable fields
-    [SerializeField]
-    private float playerSpeed = 2.0f;
-
     //Component fields
     private Player player;
 
 
     //Input Fields
     private Vector2 movementInput = Vector2.zero;
-    private bool utilElement = false;
-    private bool attackElement = false;
 
 
     //UnityEvent Methods
     public void OnMove(InputAction.CallbackContext context)
     {
         movementInput = context.ReadValue<Vector2>();
+        player.Move(movementInput);
     }
 
     public void OnUtilElement(InputAction.CallbackContext context)
     {
-        //utilElement = context.ReadValue<bool>();
-        utilElement = context.action.triggered;
+        player.UtilElement();
     }
 
     public void OnAttackElement(InputAction.CallbackContext context)
     {
-        //attackElement = context.ReadValue<bool>();
-        attackElement = context.action.triggered;
+        player.AttackElement();
     }
 
 
-    
     private void Start()
     {
         player = gameObject.GetComponent<Player>();
