7a11dcd1d605c707c1fd2b714f1ccc0fac25caf9
Rapthan <151851356+Rapthan@users.noreply.github.com>
2023-11-25 20:49:23 +0100

    Fireball patched
    Quaternion is bad, Vector is better

--- Assets/Scripts/Fireball.cs
+++ Assets/Scripts/Fireball.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -7,6 +8,12 @@ public class Fireball : MonoBehaviour, AttackElement
     [SerializeField] private float fireballSpeed = 15f;
     [SerializeField] private int countUseMax = 3;
     public GameObject fireball;
+    private Player playerScript;
+
+    public void Start()
+    {
+        playerScript=GetComponentInParent<Player>();
+    }
 
     public int getUseCount() //not used
     {
@@ -16,12 +23,13 @@ public class Fireball : MonoBehaviour, AttackElement
     public void Use()
     {
         Debug.Log("I cast FIREBALL!!!");
+        
         Transform dep = gameObject.transform;
         GameObject ball =Instantiate(fireball);
         ball.transform.position = dep.position;
         ball.transform.rotation = dep.rotation;
-        ball.transform.Translate(0,0.25f,0);
-        ball.GetComponent<Rigidbody2D>().velocity = dep.TransformDirection(fireballSpeed * Vector2.up );
+        ball.transform.Translate(playerScript.playerOrientation * 0.25f);
+        ball.GetComponent<Rigidbody2D>().velocity = dep.TransformDirection(fireballSpeed * playerScript.playerOrientation );
 
     }
 }
