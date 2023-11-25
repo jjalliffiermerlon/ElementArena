03df005fc59c4a5c7b3053095a5cec02f5053000
Aelita Le ROlland <arthur.lerolland@live.fr>
2023-11-25 12:57:48 +0100

    Add Fields to Element Manager script

--- Assets/Scripts/ElementManager.cs
+++ Assets/Scripts/ElementManager.cs
@@ -2,5 +2,31 @@ using UnityEngine;
 
 public class ElementManager : MonoBehaviour
 {
-    
+    private UtilElement utilElement;
+    private int utilElementUseLeft = 0;
+
+    private AttackElement attackElement;
+    private int attackElementUseLeft = 0;
+
+
+    public void UseUtileElement()
+    {
+        utilElement.Use();
+    }
+
+    public void UseAttackElement()
+    {
+        attackElement.Use();
+    }
+
+    //Accesseurs
+    public UtilElement GetUtilElement()
+    {
+        return utilElement;
+    }
+
+    public AttackElement GetAttackElement()
+    {
+        return attackElement;
+    }
 }
