5ec6924b5ec8b7e342da49a02b7d8b4519d6e05f
Rapthan <151851356+Rapthan@users.noreply.github.com>
2023-11-25 14:06:54 +0100

    Rotation(not finished)

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -5,19 +5,23 @@ public class Player : MonoBehaviour
 {
     // Variable modifiable sur Unity
     [SerializeField] private float speed = 10f;
-    [SerializeField] private float knockbackPunch = 10f;
+    
     private Rigidbody2D rg;
 
     // Prépare le rigidbody
     void Start()
     {
+        
         rg = gameObject.GetComponent<Rigidbody2D>();
     }
     
     // Déplace le joueur
     public void Move(Vector2 movement)
     {
+        transform.Rotate(0f, Vector2.SignedAngle(transform.forward, movement), 0f);
+        
         rg.velocity = movement * speed;
+        
     }
 
     // lance le Coup de poing
