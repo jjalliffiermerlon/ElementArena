a0590165b6d3373e790d1934adfc2029e5d61bbd
Jules Jalliffier-Merlon <72775948+jjalliffiermerlon@users.noreply.github.com>
2023-11-26 14:37:26 +0100

    cleaning UI

--- Assets/Scripts/LobbyInputManager.cs
+++ Assets/Scripts/LobbyInputManager.cs
@@ -7,6 +7,7 @@ public class LobbyInputManager : MonoBehaviour
 {
     public List<InputDevice> playersInputDevices;
     public static LobbyInputManager instance;
+    [SerializeField] private GameObject[] playerSelection;
 
     /*public void Awake()
     {
@@ -26,6 +27,29 @@ public class LobbyInputManager : MonoBehaviour
     {
         InputDevice newPlayerDevice = newPlayer.devices[0];
         playersInputDevices.Add(newPlayerDevice);
+
+        /*if (playersInputDevices.Count == 1)
+        {
+            playerSelection[1].SetActive(true);
+        }
+        else if (playersInputDevices.Count == 2)
+        {
+            playerSelection[1].SetActive(true);
+            playerSelection[2].SetActive(true);
+        }
+        else if (playersInputDevices.Count == 3)
+        {
+            playerSelection[1].SetActive(true);
+            playerSelection[2].SetActive(true);
+            playerSelection[3].SetActive(true);
+        }
+        else if (playersInputDevices.Count == 4)
+        {
+            playerSelection[1].SetActive(true);
+            playerSelection[2].SetActive(true);
+            playerSelection[3].SetActive(true);
+            playerSelection[3].SetActive(true);
+        }*/
     }
 
     public void Start()
