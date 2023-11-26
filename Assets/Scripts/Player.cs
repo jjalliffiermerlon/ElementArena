dbd9a192b1644aeebd9540238b2abd32bae5bed9
Aelita Le ROlland <arthur.lerolland@live.fr>
2023-11-26 13:35:43 +0100

    Add death sound effect

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -109,6 +109,7 @@ public class Player : MonoBehaviour
         {
             RoundManagerScript.OtherDeadPlayer = playerID;
         }
+        AudioManager.Instance.playDeathSound();
         Destroy(gameObject);
     }
 
